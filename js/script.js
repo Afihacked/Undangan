f4a6dec0484432b6bacac001bc626530d68592a2
Afitech <akhdanmuaafi68@gmail.com>
2025-07-06 20:30:40 +0700

    gf

--- js/script.js
+++ js/script.js
@@ -38,28 +38,28 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   // Autoplay setelah interaksi pengguna
-  // function tryPlayMusic() {
-  //   music
-  //     .play()
-  //     .then(() => {
-  //       isMusicPlaying = true;
-  //       toggleBtn.innerHTML = '<i class="fas fa-pause"></i>';
-  //     })
-  //     .catch((err) => {
-  //       console.warn("Autoplay diblokir:", err);
-  //     });
-  // }
+  function tryPlayMusic() {
+    music
+      .play()
+      .then(() => {
+        isMusicPlaying = true;
+        toggleBtn.innerHTML = '<i class="fas fa-pause"></i>';
+      })
+      .catch((err) => {
+        console.warn("Autoplay diblokir:", err);
+      });
+  }
 
-  // ["click", "touchstart", "scroll"].forEach((eventName) => {
-  //   window.addEventListener(
-  //     eventName,
-  //     function once() {
-  //       tryPlayMusic();
-  //       window.removeEventListener(eventName, once);
-  //     },
-  //     { once: true }
-  //   );
-  // });
+  ["click", "touchstart", "scroll"].forEach((eventName) => {
+    window.addEventListener(
+      eventName,
+      function once() {
+        tryPlayMusic();
+        window.removeEventListener(eventName, once);
+      },
+      { once: true }
+    );
+  });
 
   // === Scroll ke atas tombol ===
   const scrollBtn = document.getElementById("scroll-top");
